8846489f240388214cb378ef17d1d18f6bf3ee72
Script20 <loza00796@hotmail.co.uk>
2026-04-14 17:36:55 +0100

    fixed errors with 1.21.1

--- src/main/java/com/soaryn/chest/SoarynChest.java
+++ src/main/java/com/soaryn/chest/SoarynChest.java
@@ -7,9 +7,7 @@ import com.soaryn.chest.init.ModCreativeTabs;
 import com.soaryn.chest.init.ModItems;
 import com.soaryn.chest.client.SoarynChestConfigScreen;
 import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,20 +16,18 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class SoarynChest {
     public static final String MOD_ID = "soarynchest";
 
-    public SoarynChest() {
-        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public SoarynChest(FMLJavaModLoadingContext context) {
+        IEventBus modBus = context.getModEventBus();
 
         ModBlocks.BLOCKS.register(modBus);
         ModItems.ITEMS.register(modBus);
         ModBlockEntities.BLOCK_ENTITIES.register(modBus);
         ModCreativeTabs.CREATIVE_TABS.register(modBus);
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SoarynChestConfig.SPEC, "soarynchest-common.toml");
+        context.registerConfig(ModConfig.Type.COMMON, SoarynChestConfig.SPEC, "soarynchest-common.toml");
 
-        ModLoadingContext.get().registerExtensionPoint(
+        context.registerExtensionPoint(
                 ConfigScreenHandler.ConfigScreenFactory.class,
                 () -> new ConfigScreenHandler.ConfigScreenFactory((mc, parent) -> new SoarynChestConfigScreen(parent)));
-
-        MinecraftForge.EVENT_BUS.register(this);
     }
 }
